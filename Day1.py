438021839953d8925d1f9d5a763b86486257d21e
Tariq Mehmood <tariqmehmoodsthp23@gmail.com>
2026-01-17 12:47:30 +0500

    Introduction to Python Day1 files
    Basic input , output

---
+++ Day1.py
@@ -0,0 +1,37 @@
+print('Python successfully Installed')
+print('Hello My name is Tariq')
+
+name = "Tariq Gul"
+age=22
+fav_color='Blue'
+height=5.9
+print('My name is', name)
+print('My age is', age)
+print('My favorite color is', fav_color)
+print('My height is', height)
+print('-----------------------------------')
+name = input('Enter your name:')
+age = int(input('Enter your age:'))
+print('Hello', name, 'Your age is', age)
+
+print('-----------------------------------')
+print('Check data types:')
+print(type(name))
+print(type(age))
+print(type(fav_color))
+print(type(height))
+print('-----------------------------------')
+num1 = int(input('Enter first number:'))
+num2 = int(input('Enter second number:'))
+sum = num1 + num2
+product = num1 * num2
+print('The sum is:', sum)
+print('The product is:', product)
+print('-----------------------------------')
+print('Concatenate Strings and Number')
+years_left = 70 - age
+print('Hello ' + name + ' You have ' + str(years_left) + ' years left to live.')
+print('-----------------------------------')
+
+
+
